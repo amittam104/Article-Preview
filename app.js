b40135a18ef2fc423843906250cb0a1f9a7fedf6
Amit Tambulkar <amittambulkar104@gmail.com>
2024-01-27 16:04:57 +0530

    chore: update responsive design

--- app.js
+++ app.js
@@ -3,8 +3,18 @@
 const sharePopEl = document.querySelector(".share-pop");
 const triangleEl = document.querySelector(".triangle-down");
 const shareEl = document.querySelector(".icon--share");
+const shareMobileEl = document.querySelector(".icon--share--mobile");
 
 shareEl.addEventListener("click", function () {
   sharePopEl.classList.toggle("hidden");
   triangleEl.classList.toggle("hidden");
 });
+shareMobileEl.addEventListener("click", function () {
+  sharePopEl.classList.toggle("hidden");
+  triangleEl.classList.toggle("hidden");
+});
+
+// sharePopEl.addEventListener("click", function () {
+//   sharePopEl.classList.toggle("hidden");
+//   triangleEl.classList.toggle("hidden");
+// });
